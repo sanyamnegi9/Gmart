733bd29d9c4cfc5cce19db5a83c3e76578ba021b
Sanyam Negi <sanyamnegi@proton.me>
2023-05-01 11:38:07 +0530

    header(offer highlight, search-bar section, navbar)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import './styles/App.css';
+import React from "react";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import "./styles/App.css";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <>
-      <h1 className=''>Hello web</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
