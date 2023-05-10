17dd03d0729320f60b217dcda91e998419d695cb
Sanyam Negi <sanyamnegi@proton.me>
2023-05-10 15:49:06 +0530

    blogs and contact pages

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import BlogCard from "../components/BlogCard";
+
+const Blog = () => {
+  return (
+    <>
+      <BreadCrumb title="Blogs" />
+      <div className="blog-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Find By Catagories</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li>Watch</li>
+                    <li>Tv</li>
+                    <li>Camera</li>
+                    <li>Lapatop</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-9">
+              <div className="row">
+                <div className="col-6 mb-3">
+                  <BlogCard />
+                </div>
+                <div className="col-6 mb-3">
+                  <BlogCard />
+                </div>
+                <div className="col-6 mb-3">
+                  <BlogCard />
+                </div>
+                <div className="col-6 mb-3">
+                  <BlogCard />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Blog;
